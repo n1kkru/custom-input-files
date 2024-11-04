406b04add6a37a97cbf75c3b518226c3ec060131
Nikita Krupin <krupin.ns@yandex.ru>
2024-11-04 09:51:54 +0300

    styles: file card

--- src/main.js
+++ src/main.js
@@ -3,16 +3,49 @@ import { changeHandler, uploadDragnDrop } from './scripts/parceFiles.js'
 
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
-const dropFileZone = document.querySelector(".upload-zone");
-const previewBlock = document.querySelector('.preview');
 
+/*header*/
 const headerTitle = document.createElement("h1");
 headerTitle.textContent = 'Загрузчик';
 header.appendChild(headerTitle);
 
-dropFileZone.addEventListener("change", (e) => {changeHandler(e, previewBlock)});
+/*форма с инпутом*/
+const form = document.createElement("form");
+form.classList.add("main__form");
+form.type = 'input';
+form.enctype = "multipart/form-data";
+form.method = "post";
 
-const uploadInput = document.querySelector(".form-input");
+const label = document.createElement("label");
+label.classList.add("form-upload-zone");
+label.textContent = "";
+
+const input = document.createElement("input");
+input.classList.add("form-input");
+input.type = "file";
+input.multiple = "multiple";
+input.name = "files[]";
+
+const inputTitle = document.createElement("h2");
+inputTitle.textContent = "НАЖМИ или ПЕРЕТАЩИ";
+
+const errorBlock = document.createElement("span");
+errorBlock.classList.add("error-message");
+errorBlock.textContent = "Ошибка!"
+
+const preview = document.createElement("div");
+preview.classList.add("preview")
+
+label.append(inputTitle);
+label.append(input);
+form.append(label);
+form.append(errorBlock);
+form.append(preview);
+main.appendChild(form);
+
+
+/*слушатели*/
+label.addEventListener("change", (e) => {changeHandler(e, preview)});
 
 ["dragover", "drop"].forEach(function(event) {
   document.addEventListener(event, function(evt) {
@@ -21,26 +54,18 @@ const uploadInput = document.querySelector(".form-input");
   })
 })
 
-dropFileZone.addEventListener("dragenter", function() {
-  dropFileZone.classList.add("_active")
+label.addEventListener("dragenter", function() {
+  label.classList.add("_active")
 })
 
-dropFileZone.addEventListener("dragleave", function() {
-  dropFileZone.classList.remove("_active")
+label.addEventListener("dragleave", function() {
+  label.classList.remove("_active")
 })
 
-dropFileZone.addEventListener("drop", (e) => {
-  dropFileZone.classList.remove("_active");
+label.addEventListener("drop", (e) => {
+  label.classList.remove("_active");
   const file = e.dataTransfer?.files;
   if (file) {
-    uploadDragnDrop(file, previewBlock);
+    uploadDragnDrop(file, preview);
   }
 })
-
-uploadInput.addEventListener("change", (e) => {
-  const file = uploadInput.files?.[0];
-  if (file) {
-    uploadDragnDrop(file, previewBlock);
-  }
-})
-
--- src/scripts/parceFiles.js
+++ src/scripts/parceFiles.js
@@ -1,99 +1,120 @@
 import { formatBytes } from "./utils";
 
 function isGoodType(type) {
-  if (type == 'image/png' || type == 'image/jpg' || type == 'image/jpeg') {
-    return true
-  }
-  else {
-    return false
+  if (type == "image/png" || type == "image/jpg" || type == "image/jpeg") {
+    return true;
+  } else {
+    return false;
   }
 }
 
 function isGoodSize(size) {
   if (Number(size) <= 10485760) {
-    return true
-  }
-  else {
-    return false
+    return true;
+  } else {
+    return false;
   }
 }
 
-function checkFile(array) {
+function findGoodLength(files, block) {
+  const filesLen = files.length;
+  const blockLen = block.children.length;
+  const sum = filesLen + blockLen;
+  if (sum <= 5) return filesLen;
+  else {
+    return filesLen - (sum - 5);
+  }
+}
+
+function check(array, block) {
+  const errorBlock = document.querySelector(".error-message");
+  const goodLen = findGoodLength(array, block);
+  console.log('good len => ', goodLen, array);
+  if (array.length > goodLen) {
+    // alert(`Максимум 5 файлов!`);
+    errorBlock.textContent = "Максимум 5 файлов!";
+    errorBlock.classList.add("_active");
+  }
+  else {
+    errorBlock.classList.remove("_active");
+  }
+  array = array.slice(0, goodLen);
+  
+
   return array.filter((element) => {
     if (!isGoodSize(element.size)) {
-      alert(`${element.name} тяжеловат`)
+      alert(`${element.name} тяжеловат`);
     }
     if (!isGoodType(element.type)) {
-      alert(`${element.name} не с нашего района`)
+      alert(`${element.name} не с нашего района`);
     }
-    return (isGoodType(element.type) && isGoodSize(element.size))
+    return isGoodType(element.type) && isGoodSize(element.size);
   });
 }
 
-export function deleteCard(cardElement) {  
+export function deleteCard(cardElement) {
   cardElement.remove();
-}; 
+}
 
 export function createCard(file, handlerDelete) {
-  const cardElement = document.createElement('div');
-  cardElement.className = 'card';
-  
-  const name = document.createElement('h1');
+  const cardElement = document.createElement("div");
+  cardElement.className = "card";
+
+  const name = document.createElement("h1");
   name.textContent = file.name;
-  name.className = 'card__name';
+  name.className = "card__name";
   cardElement.append(name);
-  
-  const format = document.createElement('h2');
+
+  const format = document.createElement("h2");
   format.textContent = file.format;
-  format.className = 'card__format';
+  format.className = "card__format";
   cardElement.append(format);
-  
-  const size = document.createElement('h2');
+
+  const size = document.createElement("h2");
   size.textContent = file.size;
-  size.className = 'card__size';
+  size.className = "card__size";
   cardElement.append(size);
 
-  const image = document.createElement('img');
-  image.className = 'card__img';
+  const image = document.createElement("img");
+  image.className = "card__img";
   cardElement.append(image);
 
-  const buttonDelete = document.createElement('button');
-  buttonDelete.className = 'card__delete';
-  buttonDelete.addEventListener('click', () => {
+  const buttonDelete = document.createElement("button");
+  buttonDelete.className = "card__delete";
+  buttonDelete.addEventListener("click", () => {
     handlerDelete(cardElement);
-  }); 
+  });
   cardElement.append(buttonDelete);
 
-  return cardElement
+  return cardElement;
 }
 
 export function createCardList(array, block) {
-  array.forEach(element => {
+  array.forEach((element) => {
     const file = {
       name: element.name,
-      format: element.type.split('/')[1],
-      size: formatBytes(element.size)
-    }
+      format: element.type.split("/")[1],
+      size: formatBytes(element.size),
+    };
     const reader = new FileReader();
     reader.onload = () => {
-        const card = createCard(file, deleteCard);
-        const img = card.querySelector('.card__img');
-        img.src = reader.result;
-        img.style.display = 'block';
-        
-        block.appendChild(card);
+      const card = createCard(file, deleteCard);
+      const img = card.querySelector(".card__img");
+      img.src = reader.result;
+      img.style.display = "block";
+
+      block.appendChild(card);
     };
     reader.readAsDataURL(element);
   });
 }
 
 export function changeHandler(e, block) {
-  const array = checkFile(Array.from(e.target.files));
+  const array = check(Array.from(e.target.files), block);
   createCardList(array, block);
 }
 
 export function uploadDragnDrop(files, block) {
-
-  const array = checkFile(Array.from(files));
+  const array = check(Array.from(files), block);
   createCardList(array, block);
 }
